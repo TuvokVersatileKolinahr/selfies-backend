94e51ff0366f0fcdc850f2d7297ea92fc1db6c72
Wouter <tuvokki@gmail.com>
2014-08-20 12:09:07 +0200

    Added core option to server

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var Hapi = require('hapi');
 var selfies = require('./modules/selfies');
 
 // Server config
-var config = { };
+var config = { cors: true };
 // Create a server with a host and port
-var server = new Hapi.Server('localhost', 6523);
+var server = new Hapi.Server('localhost', 6523, config);
 
 // Add the route
 server.route(selfies);
